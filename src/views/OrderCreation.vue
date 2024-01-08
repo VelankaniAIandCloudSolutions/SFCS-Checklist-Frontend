0462d7a7d28f1362fd2b5a74123aa6a945745c36
Satvik <satvikkatoch@gmail.com>
2024-01-08 18:18:12 +0530

    increases modal size

--- src/views/OrderCreation.vue
+++ src/views/OrderCreation.vue
@@ -105,7 +105,7 @@
       aria-labelledby="exampleModalLabel"
       aria-hidden="true"
     >
-      <div class="modal-dialog">
+      <div class="modal-dialog modal-xl">
         <div class="modal-content">
           <div class="modal-header">
             <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
@@ -116,7 +116,7 @@
               aria-label="Close"
             ></button>
           </div>
-          <div class="modal-body">...</div>
+          <div class="modal-body"></div>
           <div class="modal-footer">
             <button
               type="button"

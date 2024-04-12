ddcb94817b2fa27550fdf30d63e9a94846215e70
satvik <katochsatvik@gmail.com>
2024-04-12 12:33:28 +0530

    fixed routing of ws

--- src/views/Inspection/InspectionBoards.vue
+++ src/views/Inspection/InspectionBoards.vue
@@ -92,7 +92,9 @@ export default {
   },
   mounted() {
     this.getInspectionBoards();
-    this.ws = new WebSocket(`wss://sfcsdev.xtractautomation.com/ws/checklist/`);
+    this.ws = new WebSocket(
+      `wss://sfcsdev.xtractautomation.com/ws/inspection-board/`
+    );
     // this.ws = new WebSocket(`ws://localhost:8000/ws/inspection-board/`);
     // Event listener for WebSocket connection opened
     this.ws.addEventListener("open", () => {

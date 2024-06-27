7b579cd95d5971663ef0af6ecb47dc3462d64f4a
MD Adil <md.adil@velankanigroup.com>
2024-06-27 12:31:20 +0530

    dis change commit

--- src/components/PartPricingTable.vue
+++ src/components/PartPricingTable.vue
@@ -131,7 +131,7 @@ export default {
           headerName: "Manufacturer Part No.",
         },
         {
-          field: "Online Distributor Name",
+          field: "distributor",
           headerName: "Distributor",
           cellRenderer: (params) => {
             return params.value
